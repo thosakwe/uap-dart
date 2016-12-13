e107f7967608fdad8549aef4bd28ad25080e457f
thosakwe <thosakwe@gmail.com>
2016-12-12 20:11:09 -0500

    :)

--- lib/ua_parser.dart
+++ lib/ua_parser.dart
@@ -1,1 +1,14 @@
 library ua_parser;
+
+import 'src/client.dart';
+import 'src/parser.dart';
+export 'src/client.dart';
+export 'src/device.dart';
+export 'src/os.dart';
+export 'src/parser.dart';
+export 'src/user_agent.dart';
+
+final Parser _parser = new Parser();
+
+/// Parses a header string, and returns a [Client] instance.
+Client parse(String header) => _parser.parse(header);
